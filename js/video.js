beb561b3dae43036ae051e49c86cf933b4bb3632
patam <patam@patam.(none)>
2015-09-13 19:55:11 +0200

    added videos and images
    updated castellers position
    
    added .gitignore file and reorganized images
    
    deleted videos
    
    updated images

--- js/video.js
+++ js/video.js
@@ -1,11 +1,11 @@
 $(function() 
 {
 	 $("#videoBox")
-	.css( 
-	{
-	   "background":"rgba(255,255,255,0.5)"
-	})
-	.dialog({ autoOpen: false, 
+	.dialog({ 
+		position: { my: "left top", at: "left top", of: window },
+		resizable: false,
+		width:'auto',
+		autoOpen: false, 
 		show: { effect: 'fade', duration: 500 },
 		hide: { effect: 'fade', duration: 500 },
 		close: function() { 
